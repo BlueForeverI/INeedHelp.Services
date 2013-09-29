2289495e6ffc60e63dd700f94b3d0e9f8c3fee55
GeorgiYolovski <bluef0rever@abv.bg>
2013-09-29 13:02:17 +0300

    Missed to add UserEditModel in last commit

---
+++ INeedHelp.Services/Models/UsedEditModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INeedHelp.Services.Models
+{
+    public class UsedEditModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string OldPasswordHash { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+
+        public string NewPasswordHash { get; set; }
+    }
+}
